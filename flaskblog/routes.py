5b957ba1e2217e5502bdc4662c0cc76a3e436c0b
yeyulchoi <yeyulchoi@outlook.com>
2024-02-24 14:26:29 -0500

    added logout

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,7 +3,7 @@ from flaskblog import app, db
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 from flaskblog import bcrypt
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 
 
 
@@ -36,6 +36,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data)
@@ -49,6 +51,8 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
@@ -58,3 +62,9 @@ def login():
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
