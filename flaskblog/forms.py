bc66750e714d6bcc1912ac7a2ca4efed66c3e0c8
yeyulchoi <yeyulchoi@outlook.com>
2024-02-24 10:43:51 -0500

    added to register in routes 1. bcrypt, 2 user for database and created email_validator in forms, removed Email from validator.

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,22 +1,35 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo
+from email_validator import validate_email, EmailNotValidError
+
 
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                        validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+    def validate_email(self,email):
+        try:
+            validate_email(email.data)
+        except EmailNotValidError:
+            raise EmailNotValidError("Invalid email address")
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
-                        validators=[DataRequired(), Email()])
+                        validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    def validate_email(self,email):
+        try:
+            validate_email(email.data)
+        except EmailNotValidError:
+            raise EmailNotValidError("Invalid email address")
+
