c0df05b588d5c2a02b4e0fce894e60758f08a2c7
yeyulchoi <yeyulchoi@outlook.com>
2024-02-24 12:13:53 -0500

    removed email-validator

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo
-from email_validator import validate_email, EmailNotValidError
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+
 
 
 
@@ -9,16 +9,17 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
-                        validators=[DataRequired()])
+                        validators=[DataRequired(),Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-    def validate_email(self,email):
-        try:
-            validate_email(email.data)
-        except EmailNotValidError:
-            raise EmailNotValidError("Invalid email address")
+
+
+
+
+
+
 
 
 class LoginForm(FlaskForm):
@@ -27,9 +28,4 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-    def validate_email(self,email):
-        try:
-            validate_email(email.data)
-        except EmailNotValidError:
-            raise EmailNotValidError("Invalid email address")
 
