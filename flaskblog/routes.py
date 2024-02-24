8b5c121a43fbe152fbd72d668023fabc75bb1168
yeyulchoi <yeyulchoi@outlook.com>
2024-02-24 13:55:03 -0500

    add functionality for login:flask_login

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,7 @@ from flaskblog import app, db
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
 from flaskblog import bcrypt
+from flask_login import login_user
 
 
 
@@ -50,9 +51,10 @@ def register():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user,remember=form.remember.data)
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
