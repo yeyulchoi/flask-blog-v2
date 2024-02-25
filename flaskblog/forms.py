2d36a2b338dc4637b251bc22d0d993b86768d943
yeyulchoi <yeyulchoi@outlook.com>
2024-02-25 08:32:07 -0500

    added acct update functionality

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User, Post
@@ -27,15 +28,28 @@ class RegistrationForm(FlaskForm):
 
 
 
-
-
-
-
-
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(),Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        user= User.query.filter_by(username = username.data).first()
+        if user:
+            raise ValidationError("The username is already taken. Please use different username.")
+
+    def validate_email(self, email):
+        email = User.query.filter_by(email=email.data).first()
+        if email:
+            raise ValidationError('The email entered is already taken.')
 
