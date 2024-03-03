0e68e93404a09a5b7a8a5c3fcb98b77d42bc169a
yeyulchoi <yeyulchoi@outlook.com>
2024-03-03 16:48:27 -0500

    added itsdangerous.url_safe & mail(gmail)for reset password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,9 @@
+import os  # in order to use environmental variable.
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -13,6 +15,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category='info'
-
-
+app.config['MAIL_SERVER'] ='smtp.gmail.com'
+app.config['MAIL_PORT'] =587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] ='yeyulchoi@gmail.com' #os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] ='ucta njcl aupg itqa'  #os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+#google app password: ucta njcl aupg itqa  / app name is flask_blog
 from flaskblog import routes
